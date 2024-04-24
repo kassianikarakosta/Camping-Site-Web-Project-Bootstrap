3460e00aa88d256944e9b8cc896d8ae7c9bda27e
Kassiani Karakosta <75204831+kassianikarakosta@users.noreply.github.com>
2024-04-24 17:55:50 +0300

    nav bar, footer, sign in/up and home pages

---
+++ js/sign.js
@@ -0,0 +1,8 @@
+document.getElementById('showPasswordToggle').addEventListener('click', function() {
+    var passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+});
