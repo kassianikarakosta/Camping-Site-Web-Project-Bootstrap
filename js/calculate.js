b04e5acf06c04501aad7f0fccae2e2a7ed807f69
SofiaFrg <up1083801@upnet.gr>
2024-05-13 19:09:31 +0300

    calculate ready

--- js/calculate.js
+++ js/calculate.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     // Bind calculateTotalPrice function to input event of each input field
     $('input').on('input', function() {
         calculateTotalPriceday();
+        
     });
     
     // Function to calculate total price
@@ -19,16 +20,19 @@ $(document).ready(function() {
         var threePersonsBungalow = parseInt($('#3_persons').val());
         var fourPersonsBungalow = parseInt($('#4_persons').val());
         var bellTent = parseInt($('#bell_tent').val());
-        
+        var arrivalDate = new Date($('#arrival_date').val());
+        var departureDate = new Date($('#departure_date').val());
+        var daysBetween = Math.round((departureDate - arrivalDate) / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    
         // Calculate total price based on input values
         var totalPrice = (persons * 8) + (sunnyCamping * 12) + (treeCamping * 11) + (normalCamping * 11) +
                          (sunnyCampers * 18) + (treeCampers * 17) + (normalCampers * 16) +
                          (twoPersonsBungalow * 60) + (threePersonsBungalow * 70) + (fourPersonsBungalow * 80) +
                          (bellTent * 60);
-        
+        var totalPricefinal = daysBetween*totalPrice;
         // Update total price input field
         $('#total_price_day').val(totalPrice);
-    }
-    
+        $('#total_price').val(totalPricefinal);
+    }  
     
 });
