0135432cb33ee90f631eb4b6a8c0cdb28f50b827
Kassiani Karakosta <75204831+kassianikarakosta@users.noreply.github.com>
2024-05-22 00:47:18 +0300

    change port to 3000

--- app.mjs
+++ app.mjs
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
